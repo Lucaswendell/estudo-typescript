2275dba7251481c1a7c8efdac610f8830cceea67
Lucas Wendel Costa dos Santos <wendellucas303@gmail.com>
2022-02-19 19:08:15 -0300

    Avançando em tipos: Tipo array

---
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// 1 - arrays
+let numbers = [1, 2, 3, 4];
+numbers.push(5);
+console.log(numbers[2]);
+const nomes = ["Lucas", "Wendel"];
+// 2 - outra sintaxe array
+const nums = [100, 200];
+nums.push(300);
+console.log(nums);
---
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -0,0 +1,15 @@
+// 1 - arrays
+let numbers: number[] = [1,2,3,4];
+
+numbers.push(5);
+
+console.log(numbers[2])
+
+const nomes: string[] = ["Lucas", "Wendel"];
+
+// 2 - outra sintaxe array
+const nums: Array<number> = [100, 200];
+
+nums.push(300);
+
+console.log(nums);
