74ccc6fae4bab384d1fa3606c717fc46e5492cf7
Lucas Wendel Costa dos Santos <wendellucas303@gmail.com>
2022-02-19 18:11:12 -0300

    Fundamentos de tipos

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,8 @@
+// 1 - numbers
+var x = 10;
+console.log(x);
+x = 16;
+console.log(typeof x);
+var y = 15.3456794;
+console.log(y);
+console.log(y.toPrecision(2));
---
+++ 2_FUNDAMENTOS/index.ts
@@ -0,0 +1,12 @@
+// 1 - numbers
+let x: number = 10;
+console.log(x);
+x = 16;
+
+console.log(typeof x);
+
+const y:number = 15.3456794;
+
+console.log(y);
+
+console.log(y.toPrecision(2));
