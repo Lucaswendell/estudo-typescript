b068fc6fc476f236c205be97060a0652b778200a
Lucas Wendel Costa dos Santos <wendellucas303@gmail.com>
2022-03-06 15:41:23 -0300

    Narrowing: Checagem de valor

--- 4_NARROWING/index.js
+++ 4_NARROWING/index.js
@@ -14,3 +14,22 @@ function sum(a, b) {
 sum("200", "300");
 sum(100, 10.4);
 sum("20", 2);
+// 2 - checando se o valor existe
+function operation(arr, operation) {
+    if (operation) {
+        switch (operation) {
+            case 'sum':
+                console.log(arr.reduce((valor, total) => valor + total));
+                break;
+            case "multiply":
+                console.log(arr.reduce((valor, total) => valor * total));
+                break;
+        }
+    }
+    else {
+        console.log("Por favor, defina uma operacao");
+    }
+}
+operation([1, 2, 3], "");
+operation([1, 2, 5], "sum");
+operation([1, 2, 2, 4, 5], "multiply");
