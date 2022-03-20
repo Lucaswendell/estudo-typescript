d8661b8e886be5bc3afdbb55414856f4409a7f3b
Lucas Wendel Costa dos Santos <wendellucas303@gmail.com>
2022-03-20 19:28:56 -0300

    Funções como argumento

--- 5_FUNCOES/index.js
+++ 5_FUNCOES/index.js
@@ -4,3 +4,14 @@ function withoutReturn() {
     console.log('Esta função não tem retorno');
 }
 withoutReturn();
+//2 - callback como argumento
+function greeting(name) {
+    return `Olá, ${name}`;
+}
+function preGreeting(f, userName) {
+    console.log("Preparando a função");
+    const greet = f(userName);
+    console.log(greet);
+}
+preGreeting(greeting, "Lucas");
+preGreeting(greeting, "Jose");
--- 5_FUNCOES/index.ts
+++ 5_FUNCOES/index.ts
@@ -5,3 +5,18 @@ function withoutReturn(): void{
 
 withoutReturn();
 
+//2 - callback como argumento
+function greeting(name: string): string{
+    return `Olá, ${name}`;
+}
+
+function preGreeting(f: (name: string) => string, userName: string){
+    console.log("Preparando a função");
+
+    const greet = f(userName);
+
+    console.log(greet);
+}
+
+preGreeting(greeting, "Lucas");
+preGreeting(greeting, "Jose");
