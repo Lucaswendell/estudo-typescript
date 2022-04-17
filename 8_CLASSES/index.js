698cde082db8dec933a9032f4dfe31603ee37a81
Lucaswendell <wendellucas303@gmail.com>
2022-04-17 19:07:08 -0300

    Classes: Constructor

--- 8_CLASSES/index.js
+++ 8_CLASSES/index.js
@@ -7,3 +7,13 @@ console.log(lucas);
 lucas.name = "Lucas";
 lucas.age = 10;
 console.log(lucas);
+//2 - constructor
+class NewUser {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const joao = new NewUser("João", 22);
+console.log(joao);
+// const pedro = new NewUser("Pedro", 12)
--- 8_CLASSES/index.ts
+++ 8_CLASSES/index.ts
@@ -12,3 +12,19 @@ lucas.name = "Lucas";
 lucas.age = 10;
 
 console.log(lucas);
+
+//2 - constructor
+class NewUser {
+    name;
+    age;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const joao = new NewUser("João", 22);
+console.log(joao);
+
+// const pedro = new NewUser("Pedro", 12)
