c180e226c6546758f3d3fa7cba0a11c4634a534b
Lucaswendell <wendellucas303@gmail.com>
2022-04-21 16:49:32 -0300

    React com ts: Funções

--- 11_REACT_COM_TS/src/App.tsx
+++ 11_REACT_COM_TS/src/App.tsx
@@ -7,12 +7,18 @@ function App() {
   const age: number = 20;
   const isWorking: boolean = true;
 
+  //2 - funções 
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  }
+
   return (
     <div className="App">
       <h1>Typescript com react</h1>
       <h2>Nome: {name}</h2>
       <h2>Age: {age}</h2>
       {isWorking && <p>trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
