728c7dbc5c7a3aa1ed4ae340070e8ff8b2eedd69
Lucaswendell <wendellucas303@gmail.com>
2022-04-21 13:42:47 -0300

    Decorators: Multiplos decorators

--- 10_DECORATORS/index.js
+++ 10_DECORATORS/index.js
@@ -1,8 +1,12 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 // 1 - exemplo decorator
 function myDecorator() {
@@ -14,16 +18,42 @@ function myDecorator() {
         console.log(descriptor);
     };
 }
-var MyClass = /** @class */ (function () {
-    function MyClass() {
-    }
-    MyClass.prototype.testing = function () {
+class MyClass {
+    testing() {
         console.log("Terminando execução do metodo!");
-    };
-    __decorate([
-        myDecorator()
-    ], MyClass.prototype, "testing");
-    return MyClass;
-}());
-var myClass = new MyClass();
+    }
+}
+__decorate([
+    myDecorator(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], MyClass.prototype, "testing", null);
+const myClass = new MyClass();
 myClass.testing();
+// 2 - multiplos decorators
+function a() {
+    return (target, propertyKey, descriptor) => {
+        console.log("Executou a.");
+    };
+}
+function b() {
+    return (target, propertyKey, descriptor) => {
+        console.log("Executou b.");
+    };
+}
+//Executa o mais abaixo primeiro
+class MultipleDecorators {
+    testing() {
+        console.log("Terminando execução");
+    }
+}
+__decorate([
+    b(),
+    a(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], MultipleDecorators.prototype, "testing", null);
+new MultipleDecorators().testing();
+//3
--- 10_DECORATORS/index.ts
+++ 10_DECORATORS/index.ts
@@ -25,3 +25,39 @@ class MyClass {
 const myClass = new MyClass();
 
 myClass.testing();
+
+// 2 - multiplos decorators
+
+function a(){
+  return (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) => {
+      console.log("Executou a.")
+  }
+}
+
+function b(){
+  return (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) => {
+      console.log("Executou b.")
+  }
+}
+
+//Executa o mais abaixo primeiro
+class MultipleDecorators{
+
+  @b()
+  @a()
+  testing(){
+    console.log("Terminando execução");
+  }
+}
+
+new MultipleDecorators().testing();
+
+//3
