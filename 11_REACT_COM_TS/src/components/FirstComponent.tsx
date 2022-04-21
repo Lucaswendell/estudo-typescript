ba76c01647599ae8e2e69916733025c66b8fc765
Lucaswendell <wendellucas303@gmail.com>
2022-04-21 16:57:53 -0300

    React com ts: Componentes

---
+++ 11_REACT_COM_TS/src/components/FirstComponent.tsx
@@ -0,0 +1,10 @@
+//3 - componente
+import * as React from 'react';
+
+export default function FirstComponent (): React.ReactElement {
+  return (
+    <div>
+      <h1>Meu primeiro componente</h1>
+    </div>
+  );
+}
