96096fefc6d89fecedf548c4b75280c02e8123a9
Lucaswendell <wendellucas303@gmail.com>
2022-05-01 21:25:33 -0300

    Express com TS: iniciando o express

--- 13_TS_COM_EXPRESS/src/app.ts
+++ 13_TS_COM_EXPRESS/src/app.ts
@@ -1,2 +1,14 @@
 // 1 - iniciando projeto
-console.log("Express com TS");
+//console.log("Express com TS");
+
+// 2 - init express
+import express from 'express';
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello express");
+});
+
+app.listen(3000, () => {
+    console.log("Aplicação rodando na :3000")
+})
