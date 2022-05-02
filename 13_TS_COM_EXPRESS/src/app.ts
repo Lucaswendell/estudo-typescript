a494413dd0003fe8142d1c1bb05181d7a4f731f3
Lucaswendell <wendellucas303@gmail.com>
2022-05-01 21:53:46 -0300

    Express com TS: Rota para qualquer verbo

--- 13_TS_COM_EXPRESS/src/app.ts
+++ 13_TS_COM_EXPRESS/src/app.ts
@@ -20,6 +20,17 @@ app.post("/api/product", (req, res) => {
     return res.send("Produto adicionado");
 });
 
+//4 - rota para todos os verbos
+app.all("/api/product/check", (req, res) => {
+    if(req.method === "POST"){
+        return res.send("Adicionou algo.")
+    }else if(req.method === "GET"){
+        return res.send("Leu algum resgistro.");
+    }else{
+        return res.status(405).send("Não realiza operacao.")
+    }
+});
+
 app.listen(3000, () => {
     console.log("Aplicação rodando na 3000")
 })
