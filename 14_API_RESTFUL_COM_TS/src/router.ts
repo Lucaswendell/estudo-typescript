7a4a278d3789aee1b63121c01e8663321cd687b2
Lucaswendell <wendellucas303@gmail.com>
2022-05-02 00:45:38 -0300

    API RESTful com Typescript: Criando o primeiro movie

--- 14_API_RESTFUL_COM_TS/src/router.ts
+++ 14_API_RESTFUL_COM_TS/src/router.ts
@@ -1,7 +1,10 @@
-import {Router, Request, Response} from "express";
+import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/MovieControllers";
 
 const router = Router();
 
-export default router.get('/test', (req: Request, res: Response) => {
+export default router
+  .get("/test", (req: Request, res: Response) => {
     return res.status(200).send("API WORKING");
-});
+  })
+  .post("/movie", createMovie);
