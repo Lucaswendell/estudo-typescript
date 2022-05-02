0ba164734dd1c1e4fb14968e3a3371a0dc1c29b5
Lucaswendell <wendellucas303@gmail.com>
2022-05-01 21:47:16 -0300

    Express com TS: Rota com post

--- 13_TS_COM_EXPRESS/src/app.ts
+++ 13_TS_COM_EXPRESS/src/app.ts
@@ -5,10 +5,21 @@
 import express from 'express';
 const app = express();
 
+//3 - rota com POST
+//middleware para trabalhar com json
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    return res.send("Hello express");
+    return res.json("Hello express");
+});
+
+//3 - rota com POST
+app.post("/api/product", (req, res) => {
+    console.log(req.body);
+
+    return res.send("Produto adicionado");
 });
 
 app.listen(3000, () => {
-    console.log("Aplicação rodando na :3000")
+    console.log("Aplicação rodando na 3000")
 })
