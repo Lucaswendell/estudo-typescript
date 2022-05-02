c34220982d1df6364d583edb7e078acc570ebb99
Lucaswendell <wendellucas303@gmail.com>
2022-05-01 22:03:17 -0300

    Express com TS: Enviando json

--- 13_TS_COM_EXPRESS/src/app.ts
+++ 13_TS_COM_EXPRESS/src/app.ts
@@ -2,7 +2,7 @@
 //console.log("Express com TS");
 
 // 2 - init express
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
 const app = express();
 
 //3 - rota com POST
@@ -10,33 +10,42 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    return res.json("Hello express");
+  return res.json("Hello express");
 });
 
 //3 - rota com POST
 app.post("/api/product", (req, res) => {
-    console.log(req.body);
+  console.log(req.body);
 
-    return res.send("Produto adicionado");
+  return res.send("Produto adicionado");
 });
 
 //4 - rota para todos os verbos
 app.all("/api/product/check", (req, res) => {
-    if(req.method === "POST"){
-        return res.send("Adicionou algo.")
-    }else if(req.method === "GET"){
-        return res.send("Leu algum resgistro.");
-    }else{
-        return res.status(405).send("Não realiza operacao.")
-    }
+  if (req.method === "POST") {
+    return res.send("Adicionou algo.");
+  } else if (req.method === "GET") {
+    return res.send("Leu algum resgistro.");
+  } else {
+    return res.status(405).send("Não realiza operacao.");
+  }
 });
-
 
 //5 - Interface do express
 app.get("/api/interfaces", (req: Request, res: Response) => {
-    return res.send("Utilizando interfaces");
+  return res.send("Utilizando interfaces");
+});
+
+//6 - enviando json
+app.get("/api/json", (req: Request, res: Response) => {
+    return res.json({
+        name: "Shirt",
+        price: 30.00,
+        color: "Blue",
+        sizes: ["P", "M", "G"]
+    });
 });
 
 app.listen(3000, () => {
-    console.log("Aplicação rodando na 3000")
-})
+  console.log("Aplicação rodando na 3000");
+});
